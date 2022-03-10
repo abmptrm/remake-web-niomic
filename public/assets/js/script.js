b02bd047d8ddc7737e9d52b610ced0736d6407fc
ario-bimo <adilbimoadil123@gmail.com>
2022-03-10 23:51:27 +0700

    testimoni1

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -64,5 +64,94 @@ window.onload = function() {
     infinite: true,
     speed: 300,
     autoplay: true,
-    dots: true
+    autoplaySpeed:2000
   });
+
+  // $('.testimoni-carousel').slick({
+  //   infinite: true,
+  //   slidesToShow: 2,
+  //   slidesToScroll: 2,
+  //   arrow: false,
+  //   infinite: true,
+  //   speed: 300,
+  //   autoplay: true,
+  //   autoplaySpeed:2000
+  // });
+
+  const slickSettings = {
+    // centerPadding: '-10px',
+    arrows: true,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+}
+
+// Normalize the card height.
+const handleResize = () => {
+  const carouselHeight = $('.js-slick-carousel').css('height')
+  $('.card').css('height', carouselHeight);
+}
+
+// Initialization
+const handleSlickInit = () => {
+  console.log('slick init fired');
+  handleResize();
+}
+
+$('.js-slick-carousel').on('init', handleSlickInit);
+$('.js-slick-carousel').slick(slickSettings);
+
+// reinitialization
+$('.js-slick-carousel').on('reInit', () => console.log('slick re-init fired'));
+
+$( window ).resize(handleResize);
+
+
+// $('.js-slick-carousel').slick({
+//   centerMode: true,
+//   centerPadding: '-10px',
+//   slidesToShow: 3,
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '60px',
+//         slidesToShow: 3
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '60px',
+//         slidesToShow: 1
+//       }
+//     }
+//   ]
+// });
